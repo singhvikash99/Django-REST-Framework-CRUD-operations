fd2ba13f4848f45ead0891e1b8f0a30811e3ba36
Vikash <jhonslax.bs@gmail.com>
2024-04-14 11:00:45 +0530

    Initial comit

--- blog_app/serializers.py
+++ blog_app/serializers.py
@@ -14,8 +14,8 @@ class PostSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
-        read_only_fields = ['author'] 
+        fields = fields = ['id', 'text', 'created_date', 'post', 'author']
+        read_only_fields = ['author', 'likes'] 
 
     def create(self, validated_data):
         validated_data['author'] = self.context['request'].user 
